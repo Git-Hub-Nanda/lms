1c1ef560e44e13744793c9cc1bb1a88a94757442
Ananda <anandahn110@gmail.com>
2020-01-26 11:40:58 +0530

    section changes

--- js/common.js
+++ js/common.js
@@ -2,7 +2,7 @@
  * Check the given data is Alpha Numeric or not.
  */
 function isItAAlphaNumericData(data) {
-    var validCharectors = "_abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    var validCharectors = " _abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
     for (let itr = 0; itr < data.length; itr++) {
         let currentCharector = data[itr];
         let indexOfCurrentCharector = validCharectors.indexOf(currentCharector);
@@ -55,4 +55,113 @@ function getDateAsDDMMYYYYHHMISS(){
     }
     today = day + "-" + month + "-" + year + " " + hour + ":" + minutes + ":" + seconds;
     return today;
+}
+
+function toggleMoreBooks(target) {
+    let moreblock = document.querySelector(".see-more>div");
+    if (moreblock.style.display) {
+        if (moreblock.style.display === "block") {
+            moreblock.style.display = "none";
+            target.innerText = "Show More";
+        } else {
+            moreblock.style.display = "block";
+            target.innerText = "Show Less";
+        }
+    } else {
+        moreblock.style.display = "block";
+        target.innerText = "Show Less";
+    }
+}
+
+/**
+ * Extracting frequently used books based on the requests.
+ */
+function extractFrequentlyUsedBooks() {
+    let frequentlyUsedBooks = [];
+    try {
+        let requestedBooks = localStorage.getItem("requestedBooks") ?
+            JSON.parse(localStorage.getItem("requestedBooks")) : [];
+        let bookIdsWithFrequency = {};
+        if (requestedBooks.length > 0) {
+            requestedBooks.map(r => {
+                if (bookIdsWithFrequency[r.bookId]) {
+                    bookIdsWithFrequency[r.bookId] += 1;
+                } else {
+                    bookIdsWithFrequency[r.bookId] = 1;
+                }
+            });
+        }
+        let sortedBookIdsWithFrequency = (Object.entries(bookIdsWithFrequency)
+            .map(entry => entry.reverse().join(":"))).sort().reverse();
+        console.log("bookIdsWithFrequency: ", bookIdsWithFrequency);
+        console.log("sortedBookIdsWithFrequency: ", sortedBookIdsWithFrequency);
+        let sortedBookIds = sortedBookIdsWithFrequency.map(d => d.split(":")[1]);
+        sortedBookIds.map(sbid => {
+            let books = localStorage.getItem("books") ?
+                JSON.parse(localStorage.getItem("books")) : [];
+            let fb = books.filter(bk => bk.bookId == sbid);
+            if (fb && fb.length > 0) frequentlyUsedBooks.push(fb[0]);
+        });
+        if (frequentlyUsedBooks.length >= 5)
+            frequentlyUsedBooks = frequentlyUsedBooks.slice(0, 4);
+        console.log("frequentlyUsedBooks: ", frequentlyUsedBooks);
+    } catch (exp) {
+        console.log("exp : ", exp);
+    }
+    return frequentlyUsedBooks;
+}
+
+/**
+ * Loading frequently used books.
+ */
+function loadFrequentlyUsedBooks() {
+    let promise = new Promise((resolve) => resolve(extractFrequentlyUsedBooks()));
+    promise.then(data => {
+        let frequent_books_container =
+            document.getElementById("frequent-books-container");
+        let content = "<h2>Frequently used books</h2><hr>";
+        let top2Books = '', remainingBooks = '';
+        if (data && data.length > 0) {
+            data.map((dt, index) => {
+                let rec = '<div class="frequent-books">'
+                    + '    <h3>' + initCap(dt.bookName) + '</h3>'
+                    + '    <p><strong>Author: </strong>' + initCap(dt.author) + '</p>'
+                    + '    <p><strong>Publisher: </strong>' + initCap(dt.publisher) + '</p>'
+                    + '</div>';
+                if (index < 2) {
+                    top2Books += rec;
+                } else {
+                    remainingBooks += rec;
+                }
+            });
+            content += top2Books
+                + '<div class="see-more" id="see-more"><div>';
+            if (remainingBooks != '') {
+                content += remainingBooks
+                    + '</div><button onclick="toggleMoreBooks(this);">Show More</button>';
+            }
+            content += '&nbsp;</div>';
+        } else {
+            content += "<h3>No Data Available.</h3>";
+        }
+        frequent_books_container.innerHTML = content;
+    }).catch(error => {
+        console.log("error: ", error);
+    });
+}
+
+/**
+ * 
+ * @param {*} text, this is the text in which first letter needs to be capitalized.
+ * Capitalize initial letter of the text.
+ */
+function initCap(text) {
+    if (text) {
+        if (text.length > 1) {
+            return text = text.substr(0, 1).toUpperCase() + text.substr(1);
+        } else {
+            return text.toUpperCase();
+        }
+    }
+    return text;
 }
--- js/navigation.js
+++ js/navigation.js
@@ -1,20 +1,61 @@
 function loadMenu() {
     var logindetails = sessionStorage.getItem("user-type");
     var menu = document.getElementById("menu");
-    if (logindetails == "ADMIN"  ) {
-        var adminmenucontent = "<ul>" 
+    if (logindetails == "ADMIN") {
+        var adminmenucontent = "<ul>"
             + '<li><a href="#" onclick="displayComponent(\'approval\');">APPROVALS</a></li>'
             + '<li><a href="#" onclick="displayComponent(\'add\');">ADD BOOK</a></li>'
             + '<li><a href="#" onclick="displayComponent(\'edit\');">EDIT BOOK</a></li>'
             + '<li><a href="#" onclick="displayComponent(\'delete\');">DELETE BOOK</a></li>'
-        + "</ul>";
+            + "</ul>";
         menu.innerHTML = adminmenucontent;
     } else {
-        var studentmenucontent = '<ul>' 
+        var studentmenucontent = '<ul>'
             + '<li><a href="#" onclick="displayComponent(\'request\');">REQUEST BOOK</a></li>'
             + '<li><a href="#" onclick="displayComponent(\'return\');">MY REQUESTS</a></li>'
-        + '</ul>';
-        menu.innerHTML = studentmenucontent; 
+            + '</ul>';
+        menu.innerHTML = studentmenucontent;
+    }
+}
+
+function loadSection() {
+    var userType = sessionStorage.getItem("user-type");
+    var section = document.getElementById("sec1");
+    if (userType == "STUDENT") {
+        var studentsection = '<h2>Available Options</h2>'
+            + '<ul>'
+            + '    <li><strong>Request Book</strong>'
+            + '        <p><ul><li>Book request creation.</li></ul></p>'
+            + '    </li>'
+            + '    <li><strong>Return Book</strong>'
+            + '        <p><ul><li>Returning the book after its being used.</li></ul></p>'
+            + '    </li>'
+            + '    <li><strong>Close Request</strong>'
+            + '        <p><ul><li>Request can be closed after the book return is accepted by the admin.</li></ul></p>'
+            + '    </li>'
+            + '</ul><br>';
+        section.innerHTML = studentsection;
+    } else {
+        var adminsection = '<h2>Available Options</h2>'
+            + '<ul>'
+            + '    <li><strong>Add Book</strong>'
+            + '        <p><ul><li>A new book will be added to the system.</li></ul></p>'
+            + '    </li>'
+            + '    <li><strong>Edit Book</strong>'
+            + '        <p><ul><li>Existing book will be edited updated to the system.</li></ul></p>'
+            + '    </li>'
+            + '    <li><strong>Delete Book</strong>'
+            + '        <p><ul><li>Existing book will be deleted from the system.</li></ul></p>'
+            + '    </li>'
+            + '    <li><strong>Approvals</strong>'
+            + '        <p><ul>'
+            + '            <li>Book request approval.</li>'
+            + '            <li>Book request Rejection.</li>'
+            + '            <li>Book return Acceptance.</li>'
+            + '        </ul></p>'
+            + '    </li>'
+            + '</ul><br>';
+        section.innerHTML = adminsection;
     }
 }
 
